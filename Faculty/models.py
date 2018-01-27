2a00c8795e8f794815dc75fa6123566ec2eec43f
cabilangan112 <jassencabilangan@gmail.com>
2018-01-27 21:26:56 +0800

    Faculty Models

--- Faculty/models.py
+++ Faculty/models.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from datetime import date
+from datetime import time
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import pre_save
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class detail(models.Model):
+	Last_name    		 = models.CharField(max_length=200)
+	First_name			 = models.CharField(max_length=200)	
+	Subject				 = models.CharField(max_length=200)
+	Evaluation_Date      = models.DateField(default=date.today)
+	Time			     = models.TimeField(null=True, blank=True)
+	
+	
+	def __str__(self):
+		return self.Last_name
+	
+	def get_absolute_url(self):
+		return reverse('detail', args=[str(self.id)])
--- Question/models.py
+++ Question/models.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
+from Faculty.models import detail
 
-# Create your models here.
+class Question(models.Model):
+	Name_of_Faculty = models.ForeignKey(detail, on_delete=models.CASCADE)
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	comment = models.TextField()
+
+	def __str__(self):
+		return self.question_text
+		
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		
+		
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+	
+    def __str__(self):
+        return self.choice_text
